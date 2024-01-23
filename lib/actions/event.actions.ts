10f7b912b1f7dc1132e5bd415927ef859163d67a
Hoang Tran <tdll.trdphoang@gmail.com>
2024-01-23 15:15:48 -0800

    Finished getEventById

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -6,6 +6,17 @@ import User from "../database/models/user.model";
 import { revalidatePath } from "next/cache";
 import { handleError } from "../utils";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+const populateEvent = async (query: any) => {
+	return query
+		.populate({
+			path: "organizer",
+			model: User,
+			select: "_id firstName lastName",
+		})
+		.populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export async function createEvent({ userId, event, path }: CreateEventParams) {
 	try {
@@ -26,3 +37,19 @@ export async function createEvent({ userId, event, path }: CreateEventParams) {
 		handleError(error);
 	}
 }
+
+export const getEventById = async (eventId: string) => {
+	try {
+		await connectToDatabase();
+
+		const event = await populateEvent(Event.findById(eventId));
+
+		if (!event) {
+			throw new Error("Event not found");
+		}
+
+		return JSON.parse(JSON.stringify(event));
+	} catch (error) {
+		handleError(error);
+	}
+};
